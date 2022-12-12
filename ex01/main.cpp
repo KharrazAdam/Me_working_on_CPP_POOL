2214e047523839c6b2985c30fe920a4746f85737
KharrazAdam <adam.kharraz13@gmail.com>
2022-12-12 12:28:22 +0100

    thanks to jacob

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,28 +6,37 @@
 /*   By: akharraz <akharraz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 01:05:15 by akharraz          #+#    #+#             */
-/*   Updated: 2022/12/12 08:59:20 by akharraz         ###   ########.fr       */
+/*   Updated: 2022/12/12 12:24:52 by akharraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "PhoneBook.hpp"
+# include "PhoneBook.hpp"
+# include <unistd.h>
 
 int main(void)
 {
 	PhoneBook   phonebook;
 	std::string   command;
 	int i = -1;
+	
 	while (1)
 	{
-        std::cout << "insert ADD or SEARCH or Exit: ";
-		getline(std::cin, command);
+		if (std::cin.eof() == 1)
+		{
+			std::cin.clear();
+			clearerr(stdin);
+			std::cout << std::endl;
+			continue;
+		}
+		std::cout << "insert ADD or SEARCH or Exit: ";
+		std::getline(std::cin, command);
 		if (!command.compare("ADD"))
 			phonebook.set(++i % MAX_CONTACTS);
 		else if (!command.compare("SEARCH"))
 			phonebook.get(i);
 		else if (!command.compare("EXIT"))
-            return (0);
+			return (0);
 	}
 	return (0);
 }
