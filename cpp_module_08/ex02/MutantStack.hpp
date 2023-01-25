faba161c0dd69dff94221a8809024b6cd36102ce
KharrazAdam <adam.kharraz13@gmail.com>
2023-01-25 15:58:33 +0100

    zakaria finished ex02

--- cpp_module_08/ex02/MutantStack.hpp
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -6,10 +6,47 @@
 /*   By: akharraz <akharraz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 09:39:20 by akharraz          #+#    #+#             */
-/*   Updated: 2023/01/25 14:29:44 by akharraz         ###   ########.fr       */
+/*   Updated: 2023/01/25 15:55:46 by akharraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
+#include <list>
+#include <vector>
+#include <deque>
 
+template <typename T, class Container = std::deque<T> > class MutantStack : public std::stack<T, Container>
+{
+public:
+    // constructors
+    typedef typename Container::iterator iterator;
+    MutantStack(/* args */)
+    {
+        std::cout << "default constructor call" <<std::endl;
+    }
+    MutantStack(const MutantStack& obj)
+    {
+        (*this) = obj;
+        std::cout << "default constructor call" <<std::endl;
+    }
+    // copy assignment operator
+    MutantStack& operator =(const MutantStack& obj)
+    {
+        this->c = obj.c;
+        return (*this);
+    }
+    // Destructor
+    ~MutantStack()
+    {
+        std::cout << "destructor call" <<std::endl;  
+    }
+    iterator begin()
+    {
+        return (this->c.begin());
+    }
+    iterator end()
+    {
+        return (this->c.end());
+    }
+};
